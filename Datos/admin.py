9ff962a91338842e0eecaae4b000ddf9b338cc0b
JUAN PABLO <juanpablo@MacBook-Pro-de-JUAN.local>
2017-12-14 10:32:38 -0500

    Export

--- Datos/admin.py
+++ Datos/admin.py
@@ -2,7 +2,12 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportActionModelAdmin
 from .models import *
 # Register your models here.
 
-admin.site.register(Dato)
+class AdminExport(ImportExportActionModelAdmin):
+    pass
+
+admin.site.register(Dato, AdminExport)
